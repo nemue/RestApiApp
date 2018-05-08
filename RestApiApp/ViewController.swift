3389434865877228f5b562471b8422e240838364
Nele Müller <nm@fp.de>
2018-05-08 15:21:03 +0200

    Initial Commit

---
+++ RestApiApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RestApiApp
+//
+//  Created by Nele Müller on 08.05.18.
+//  Copyright © 2018 Nele Müller. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
